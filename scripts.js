4656f03dd6a8b862a5fbdec4ede901bf512f16cf
Teibok Chyne <smartwork00001@gmail.com>
2023-01-30 19:04:23 +0530

    Add button to resize the grid
    Take number of grids per line from user and make size of each grid such that they occupy the container size fully

--- scripts.js
+++ scripts.js
@@ -1,17 +1,43 @@
 const body = document.querySelector("body");
 const grids = document.querySelector(".grids");
+const btn = document.querySelector(".btn");
 
-for(let i=0;i<16;++i){
-    for(let j=0;j<16;++j){
-        let grid = document.createElement("div");
-        grid.classList.add("grid");
-        grids.appendChild(grid);
+function generateGrids(n){
+    while(grids.lastChild){
+        grids.removeChild(grids.lastChild);
     }
+    for(let i=0;i<n;++i){
+        for(let j=0;j<n;++j){
+            let grid = document.createElement("div");
+            grid.classList.add("grid");
+            grids.appendChild(grid);
+        }
+    }
+
+    let gridElements = document.getElementsByClassName("grid");
+    let percent = 100/n;
+    for(let i=0; i<gridElements.length; ++i){
+        gridElements[i].style.width = `${percent}%`;
+    }
+
+    /*Add event listener to these grids */
+    const gridNodeList = document.querySelectorAll(".grid");
+    gridNodeList.forEach(grid => grid.addEventListener('mouseover', addHoverClass));
 }
 
 function addHoverClass(e){
     this.classList.add('hovered');
 }
 
-const gridNodeList = document.querySelectorAll(".grid");
-gridNodeList.forEach(grid => grid.addEventListener('mouseover', addHoverClass))
+function buttonClick(e){
+    let numberSquarePerSide = parseInt(prompt("Enter no of squares per side"));
+    if(isNaN(numberSquarePerSide) || numberSquarePerSide<1 || numberSquarePerSide>100){
+        alert("Please enter a number between 1 and 100");
+    }else{
+        generateGrids(numberSquarePerSide);
+    }
+}
+
+
+
+btn.addEventListener('click', buttonClick);
